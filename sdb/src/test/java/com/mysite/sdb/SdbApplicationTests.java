5d38036cb89cf49a80c7ece4b3d9008e7aa24640
ohytyoon <ohyt920118@gmail.com>
2023-01-03 15:16:32 +0900

    깃실습

---
+++ sdb/src/test/java/com/mysite/sdb/SdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
